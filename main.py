d9f75445035d012eaaae7d9f801df76589c81e58
razmag <razmagorii@gmail.com>
2022-02-17 00:33:41 +0200

    refactor, I hope it still works.

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-# from xxlimited import new
+import os
+import re
+import csv
+import glob
+
+from time import ctime as time
+from os import getenv
+from os import getcwd
+from pathlib import Path
+from dotenv import load_dotenv
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
@@ -6,114 +16,96 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.chrome.service import Service as ChromeService
-
-from time import ctime as time
-from os import getenv
-from os import getcwd
-import os
-from dotenv import load_dotenv
-import re
-import csv
-from pathlib import Path
-import glob
-
-
-load_dotenv()
-
 
 
 def main():
-    print(f"{time()} - Running!")
+    logger("Running!")
+    load_dotenv()
     Path(f"{getcwd()}/temp/").mkdir(parents=True, exist_ok=True)
-    # service = ChromeService(executable_path='/usr/bin/chromedriver')
-    # caps = webdriver.DesiredCapabilities.CHROME.copy() 
-    # caps['download.default_directory'] = f"{getcwd()}/temp"
-    # chrome_options = webdriver.ChromeOptions()
-    # prefs = {'download.default_directory' : f"{getcwd()}/temp"}
-    # chrome_options.add_experimental_option('prefs', prefs)
-    
-    # options.set_preference("browser.download.dir",f"{getcwd()}/temp")
-    
-    # options.set_preference("browser.download.viewableInternally.enabledTypes","")
-    # desired_capabilities=caps
-    options = Options()
-    fp = FirefoxProfile(f"{getcwd()}/firefox_profile/")
-    options.profile = fp
-    options.set_preference("browser.download.dir",f"{getcwd()}/temp")
-    options.set_preference("browser.download.folderList",2)
-    options.set_preference("browser.helperApps.neverAsk.saveToDisk","Application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-    options.set_preference("browser.download.manager.showWhenStarting",False)
+    log_file = open('scrpr.log', 'w')
+    log_file.close()
+    options = set_firefox_options()
 
     driver = webdriver.Firefox(options=options)
-    # driver.implicitly_wait(1)
     driver.get("http://rishum.mta.ac.il/")
-    u_name = getenv('MTA_USERNAME')
-    pw = getenv('MTA_PASSWORD')
+    username = getenv('MTA_USERNAME')
+    password = getenv('MTA_PASSWORD')
+    db_file = ('courses.csv')
 
-    courses = []
-    with open('courses.csv') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            for item in row:
-                courses.append(str(item))
-    login(driver, u_name, pw)
+    courses = ingest_courses(db_file)
+    login(driver, username, password)
     goto_exam_bank_page(driver)
-    downloaded_exams = []
     for course in courses:
         get_course_page_by_code(driver, course)
-        table = driver.find_element(By.CSS_SELECTOR, "#myTable0")
-        for table_row in table.find_elements(By.CSS_SELECTOR, 'tbody>tr'):
-            this_row = table_row.find_elements(By.CSS_SELECTOR, 'td')
-            test = {
-                "year": this_row[5].text,
-                "sem": this_row[6].text,
-                "sitting": this_row[7].text
-            }
-            if test not in downloaded_exams:
-                cur_year = re.sub("[^0-9]", "", test['year'])
-                if 'א' in test['sem']:
-                    cur_sem = 1
-                elif 'ב' in test['sem']:
-                    cur_sem = 2
-                else:
-                    cur_sem = 3
-                Path(f"{getcwd()}/output/{course}/{cur_year}/{cur_sem}").mkdir(parents=True, exist_ok=True)
-                cur_path = f"{getcwd()}/output/{course}/{cur_year}/{cur_sem}"
-                download_btn_cell = this_row[9].find_elements(By.CSS_SELECTOR,'input[value="הורדה"]')
-                if (len(download_btn_cell)>0):
-                    driver.execute_script("arguments[0].click();", download_btn_cell[0])
-                else:
-                    window_before = driver.window_handles[0]
-                    secondary_page_download_btn = this_row[10].find_elements(By.CSS_SELECTOR,'input[value="קבצים נוספים"]')
-                    driver.execute_script("arguments[0].click();", secondary_page_download_btn[0])
-                    window_after = driver.window_handles[len(driver.window_handles)-1]
-                    driver.switch_to.window(window_after)
-                    waitfor(driver,By.XPATH,"//button[contains(text(),'קישור לקובץ')]")
-                    download_btn = driver.find_elements(By.XPATH,"//button[contains(text(),'קישור לקובץ')]")
-                    for btn in download_btn:
-                        driver.execute_script("arguments[0].click();",btn)
+        table = driver.find_elements(By.CSS_SELECTOR, "#myTable0")
+        if len(table) < 1:
+            continue
+        download_course(driver, table, course)
+
+
+def download_course(driver, table, course):
+    downloaded_exams = []
+    for table_row in table.find_elements(By.CSS_SELECTOR, 'tbody>tr'):
+        this_row = table_row.find_elements(By.CSS_SELECTOR, 'td')
+        test = {
+            "year": this_row[5].text,
+            "sem": this_row[6].text,
+            "sitting": this_row[7].text
+        }
+        if test not in downloaded_exams:
+            download_exam(driver, course, this_row, test)
+            downloaded_exams.append(test)
+    logger(f"Finished {course}")
+
+
+def download_exam(driver, course, this_row, test):
+    cur_year = re.sub("[^0-9]", "", test['year'])
+    if 'א' in test['sem']:
+        cur_sem = 1
+    elif 'ב' in test['sem']:
+        cur_sem = 2
+    else:
+        cur_sem = 3
+    Path(f"{getcwd()}/output/{course}/{cur_year}/{cur_sem}").mkdir(parents=True, exist_ok=True)
+    cur_path = f"{getcwd()}/output/{course}/{cur_year}/{cur_sem}"
+    download_btn_cell = this_row[9].find_elements(
+        By.CSS_SELECTOR, 'input[value="הורדה"]')
+    if len(download_btn_cell) > 0:
+        driver.execute_script("arguments[0].click();", download_btn_cell[0])
+    else:
+        window_before = driver.window_handles[0]
+        secondary_page_download_btn = this_row[10].find_elements(
+            By.CSS_SELECTOR, 'input[value="קבצים נוספים"]')
+        driver.execute_script(
+            "arguments[0].click();", secondary_page_download_btn[0])
+        window_after = driver.window_handles[len(driver.window_handles)-1]
+        driver.switch_to.window(window_after)
+        waitfor(driver, By.XPATH, "//button[contains(text(),'קישור לקובץ')]")
+        download_btn = driver.find_elements(
+            By.XPATH, "//button[contains(text(),'קישור לקובץ')]")
+        for btn in download_btn:
+            driver.execute_script("arguments[0].click();", btn)
+        driver.close()
+        driver.switch_to.window(window_before)
+        while len(driver.window_handles) > 1:
+            driver.implicitly_wait(1)
+            if len(driver.window_handles) > 1:
+                driver.switch_to.window(driver.window_handles[1])
+                driver.implicitly_wait(1)
+                if len(driver.window_handles) > 1:
                     driver.close()
-                    driver.switch_to.window(window_before)
-                    while(len(driver.window_handles)>1):
-                        driver.implicitly_wait(1)
-                        if(len(driver.window_handles)>1):
-                            driver.switch_to.window(driver.window_handles[1])
-                            driver.implicitly_wait(1)
-                            if(len(driver.window_handles)>1):
-                                driver.close()
-                ready_to_move = False
-                while(not ready_to_move):
-                    download_fileName = glob.glob(f'{getcwd()}/temp/*.*')
-                    if  len(download_fileName)>0:
-                        extension = os.path.splitext(download_fileName[0])
-                        if '.part' not in extension[1] : ready_to_move = True
-                    driver.implicitly_wait(0.5)
-                os.system(f"mv {getcwd()}/temp/* {cur_path}")
-                print(f"{time()} - downloaded {course} - {cur_year} / {test['sem']} / {test['sitting']}")
-                downloaded_exams.append(test)
-                driver.switch_to.window(driver.window_handles[0])
-        print(f"{time()} - finished {course}")
+    ready_to_move = False
+    while not ready_to_move:
+        download_file_name = glob.glob(f'{getcwd()}/temp/*.*')
+        if len(download_file_name) > 0:
+            extension = os.path.splitext(download_file_name[0])
+            if '.part' not in extension[1]:
+                ready_to_move = True
+        driver.implicitly_wait(0.5)
+    os.system(f"mv {getcwd()}/temp/* {cur_path}")
+    logger(
+        f"Downloaded {course} - {cur_year} / {test['sem']} / {test['sitting']}")
+    driver.switch_to.window(driver.window_handles[0])
 
 
 def get_course_page_by_code(driver, code):
@@ -137,7 +129,17 @@ def get_course_page_by_code(driver, code):
         show_all_btn = driver.find_element(
             By.XPATH, "//li[contains(text(),'הכל')]")
         show_all_btn.click()
-    print(f"{time()} - Got course nubmer {code}")
+    logger(f"Got course nubmer {code}")
+
+
+def ingest_courses(db_file):
+    courses = []
+    with open(db_file) as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            for item in row:
+                courses.append(str(item))
+    return courses
 
 
 def waitfor(driver, elem_type, elem_name):
@@ -146,15 +148,29 @@ def waitfor(driver, elem_type, elem_name):
         EC.presence_of_element_located((elem_type, elem_name)))
 
 
-def login(driver, u_name, pw):
+def set_firefox_options():
+    options = Options()
+    options.profile = FirefoxProfile(f"{getcwd()}/firefox_profile/")
+    options.set_preference("browser.download.dir", f"{getcwd()}/temp")
+    options.set_preference("browser.download.folderList", 2)
+    options.set_preference("browser.helperApps.neverAsk.saveToDisk",
+                            "Application/pdf, application/msword, \
+                            application/vnd.openxmlformats-officedocument.wordprocessingml.document,\
+                            application/vnd.ms-excel, \
+                            application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    options.set_preference("browser.download.manager.showWhenStarting", False)
+    return options
+
+
+def login(driver, username, password):
     waitfor(driver, By.ID, "Ecom_User_ID")
     username_box = driver.find_element(By.ID, "Ecom_User_ID")
     pw_box = driver.find_element(By.ID, "Ecom_Password")
     login_btn = driver.find_element(By.ID, "loginButton2")
-    username_box.send_keys(u_name)
-    pw_box.send_keys(pw)
+    username_box.send_keys(username)
+    pw_box.send_keys(password)
     login_btn.click()
-    print(f"{time()} - Logged in")
+    logger("Logged in")
 
 
 def goto_exam_bank_page(driver):
@@ -162,7 +178,14 @@ def goto_exam_bank_page(driver):
     exam_bank_btn = driver.find_element(
         By.XPATH, "//*[contains(text(),'מאגר בחינות')]/..")
     driver.execute_script("arguments[0].click();", exam_bank_btn)
-    print(f"{time()} - Got exam page")
+    logger("Got exam page")
+
+
+def logger(message):
+    message = f"{time()} - {message}"
+    print(message)
+    with open('scrpr.log', 'a') as log_file:
+        log_file.write(message)
 
 
 if __name__ == "__main__":
